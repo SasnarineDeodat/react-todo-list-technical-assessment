fd5d0af68faebf9a5731d1ea40af1803fb8c568d
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-19 20:40:17 -0500

    Built out the HomePage component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
   return (
-    <div>
-      <h1 className="text-4xl">Todo App</h1>
+    <div className="min-h-screen bg-black flex flex-col justify-center items-center">
+      <h1 className="text-center text-5xl md:text-6xl font-bold text-green-500">
+        Welcome to the Todo App
+      </h1>
+      <p className="text-center text-xl md:text-2xl text-green-500 mt-4">
+        Technical Interview
+        <br />
+        <Link href="/todo">
+          <button className="bg-green-500 text-white px-8 py-2 rounded hover:bg-green-800 transition-colors duration-200 mt-4">
+            Go to To-Do List →
+          </button>
+        </Link>
+      </p>
     </div>
   );
 };
