a3d29dc5406ac21dee7efb9f47481ba931d8b617
Robbie213 <sasnarinedeodat407@gmail.com>
2024-02-19 20:39:14 -0500

    Add the routing system with createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Todo from "./components/Todo";
 
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/todo", element: <Todo /> },
+]);
+
 const App = () => {
-  return (
-    <div>
-      <HomePage />
-      <Todo />
-    </div>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 };
 export default App;
